d5d31f2a7cd9155daac462168279e77adf9413b9
Aleksey203SkyUp <93546814+Aleksey203SkyUp@users.noreply.github.com>
2022-03-06 13:37:33 +0200

    Update BreadcrumbsManager.php

--- classes/BreadcrumbsManager.php
+++ classes/BreadcrumbsManager.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DaveJamesMiller\Breadcrumbs;
+namespace Aleksey203SkyUp\Breadcrumbs;
 
 use DaveJamesMiller\Breadcrumbs\Exceptions\DuplicateBreadcrumbException;
 use DaveJamesMiller\Breadcrumbs\Exceptions\InvalidBreadcrumbException;
